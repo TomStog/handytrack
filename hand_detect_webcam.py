d0b74a61dfcf7f40c000628276af4a2812a240d6
Thomas Stogiannopoulos <82231949+TomStog@users.noreply.github.com>
2023-04-18 01:35:35 +0300

    Update hand_detect_webcam.py

--- hand_detect_webcam.py
+++ hand_detect_webcam.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
-mp_myhand = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+mp_myhand = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
 webcam = cv2.VideoCapture(0)
 
@@ -14,10 +14,10 @@ while webcam.isOpened():
     results = mp_myhand.process(img)
 
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-    x_min, x_max, y_min, y_max = float('inf'), 0, float('inf'), 0
+    
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
+            x_min, x_max, y_min, y_max = float('inf'), 0, float('inf'), 0
             for landmark in hand_landmarks.landmark:
                 x, y = int(landmark.x * img.shape[1]), int(landmark.y * img.shape[0])
                 if x < x_min:
@@ -30,7 +30,7 @@ while webcam.isOpened():
                     y_max = y
                 mp_drawing.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-        cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+            cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
 
     cv2.imshow('Hand Tracking', img)
     #print(x_min, y_min,x_max, y_max)
