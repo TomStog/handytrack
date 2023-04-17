2c63ef439d7b9cdb3c83f3a93b94fbeb669cad28
Thomas Stogiannopoulos <82231949+TomStog@users.noreply.github.com>
2023-04-18 01:37:19 +0300

    Update hand_detect_webcam_frame.py

--- hand_detect_webcam_frame.py
+++ hand_detect_webcam_frame.py
@@ -12,13 +12,13 @@ webcam = cv2.VideoCapture(0)
 success, img = webcam.read()
 
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)	
-results = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.5).process(img)
+results = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5).process(img)
 
 img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-x_min, x_max, y_min, y_max = float('inf'), 0, float('inf'), 0
 if results.multi_hand_landmarks:
     for hand_landmarks in results.multi_hand_landmarks:
+        x_min, x_max, y_min, y_max = float('inf'), 0, float('inf'), 0
         for landmark in hand_landmarks.landmark:
             x, y = int(landmark.x * img.shape[1]), int(landmark.y * img.shape[0])
             if x < x_min:
@@ -31,7 +31,7 @@ if results.multi_hand_landmarks:
                 y_max = y
             mp_drawing.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-    cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+        cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
 
 # Define the data to write to file
 data = [[x_min, y_min,x_max, y_max]]
